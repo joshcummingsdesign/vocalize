7d11cf92ea32ee3a90732417bf1aee5d52626d4f
Josh Cummings <joshcummingsdesign@gmail.com>
2022-08-15 12:05:23 -0700

    basic vim nagivation

--- grammar/_vim.py
+++ grammar/_vim.py
@@ -1,4 +1,4 @@
-from dragonfly import Grammar, MappingRule, Key, IntegerRef, Function
+from dragonfly import Grammar, MappingRule, Key, IntegerRef, Function, Text
 
 
 class Vim:
@@ -11,13 +11,21 @@ class Vim:
             mapping={
                 'in': Key('i') + Function(lambda: print('i')),
                 'out': Key('escape') + Function(lambda: print('escape')),
-                '[<n>] slap': Key('%(n)d,o') + Function(lambda n: print(f'{n}o'), extra={'n'}),
-                '[<n>] bump': Key('%(n)d,O') + Function(lambda n: print(f'{n}O'), extra={'n'}),
+                '[<n>] slap': Key('o:%(n)d') + Function(lambda n: print(f'{n}o'), extra={'n'}),
+                '[<n>] bump': Key('O:%(n)d') + Function(lambda n: print(f'{n}O'), extra={'n'}),
                 'wipe': Key('d, d') + Function(lambda: print('dd')),
                 'save': Key('colon, w, enter') + Function(lambda: print(':w enter')),
                 'quit': Key('colon, q, enter') + Function(lambda: print(':q enter')),
                 'undo': Key('u') + Function(lambda: print('u')),
                 'redo': Key('c-r') + Function(lambda: print('c-r')),
+                'top': Key('g,g') + Function(lambda: print('gg')),
+                'bottom': Key('G') + Function(lambda: print('G')),
+                'zed zed': Key('z,z') + Function(lambda: print('zz')),
+                'zed top': Key('z,t') + Function(lambda: print('zt')),
+                'zed bottom': Key('z,b') + Function(lambda: print('zb')),
+                '[<n>] up': Key('k:%(n)d') + Function(lambda n: print(f'{n}k'), extra={'n'}),
+                '[<n>] down': Key('j:%(n)d') + Function(lambda n: print(f'{n}j'), extra={'n'}),
+                'line <n>': Key('colon') + Text('%(n)d') + Key('enter') + Function(lambda n: print(f':{n} enter'), extra={'n'}),
             },
             extras=[
                 IntegerRef("n", 1, 1000),
