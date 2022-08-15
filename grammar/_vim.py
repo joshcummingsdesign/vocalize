38d8e53fa15db6cb40fb6d6de0f5da33c305bc0b
Josh Cummings <joshcummingsdesign@gmail.com>
2022-08-14 17:25:48 -0700

    remove dictate grammar

--- grammar/_vim.py
+++ grammar/_vim.py
@@ -3,12 +3,12 @@ from dragonfly import Grammar, MappingRule, Key
 
 class VimRule(MappingRule):
     mapping = {
-        "in": Key('i'),
-        "out": Key('escape'),
-        "slap": Key('o'),
-        "wipe": Key('d, d'),
-        "save": Key('colon, w, enter'),
-        "quit": Key('colon, q, enter'),
+        'in': Key('i'),
+        'out': Key('escape'),
+        'slap': Key('o'),
+        'wipe': Key('d, d'),
+        'save': Key('colon, w, enter'),
+        'quit': Key('colon, q, enter'),
     }
 
 
